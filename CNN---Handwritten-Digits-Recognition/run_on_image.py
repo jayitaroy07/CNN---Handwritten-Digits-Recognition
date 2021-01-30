cda9ff279a5c9e3fabac81b7d40d5bb09f022f55
Jayita Roy <71405404+jayitaroy07@users.noreply.github.com>
2021-01-30 12:19:02 -0500

    Add files via upload

---
+++ CNN---Handwritten-Digits-Recognition/run_on_image.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+import numpy as np
+
+from nn import *
+import cv2
+
+
+def run_on_image(img_path, model_path):
+    net = NN(mode='test', model_path=model_path)
+
+    img = cv2.imread(img_path)
+
+    img = img[:, :, 0].astype(np.float32) / 255.0 - 0.5
+    img = img.reshape(1, 784)
+
+    cache = net.forward(img)
+    probs = cache[-2]
+
+    label = np.argmax(probs, axis=1)
+
+    print("Predicted Label {}".format(label[0]))
+
+
+if __name__ == '__main__':
+    img_path = sys.argv[1]
+    model_path = sys.argv[2]
+    run_on_image(img_path, model_path)
